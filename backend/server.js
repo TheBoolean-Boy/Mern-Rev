7df085d0f030ad5390bef30a240f2898dd8f1426
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-20 13:30:55 +0530

    request validation integrated

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready");
+app.post("/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.image || !product.price) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
