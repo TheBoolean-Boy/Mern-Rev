b894017efad5849d1b12fc3f6fe3bb809a35cb51
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-20 20:24:13 +0530

    Navbar and darkmode toggle functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Create from "./pages/Create";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
-      <Box minH={"100vh"}>
+    
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
         {/* NAVBAR */}
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
+          <Route path="/create" element={<Create />}/>
         </Routes>
       </Box>
+
     </div>
   );
 }
