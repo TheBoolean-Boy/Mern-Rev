39794eb31ad1ccbe4f36d75ffbc666cb039dbb25
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-20 11:52:47 +0530

    Database connection extablished

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-import express, { response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/home", (req, res) => {
+app.get("/products", (req, res) => {
   res.send("Server is ready");
 });
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 });
